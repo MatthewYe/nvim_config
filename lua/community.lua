4f8ca4689c3e1144559ac77272baa0c0b34032a2
MatthewYe <sssyeke@gmail.com>
2024-06-20 12:45:29 +0800

    feat: add commnunity plugin in need

--- lua/community.lua
+++ lua/community.lua
@@ -1,12 +1,15 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
 -- AstroCommunity: import any community modules here
 -- We import this file in `lazy_setup.lua` before the `plugins/` folder.
 -- This guarantees that the specs are processed before any user plugins.
 
 ---@type LazySpec
 return {
-  "AstroNvim/astrocommunity",
-  { import = "astrocommunity.pack.lua" },
-  -- import/override with your plugins folder
+	"AstroNvim/astrocommunity",
+	{ import = "astrocommunity.pack.lua" },
+	-- import/override with your plugins folder
+	{ import = "astrocommunity.pack.rust" },
+	{ import = "astrocommunity.pack.markdown" },
+	{ import = "astrocommunity.colorscheme.tokyonight-nvim" },
+	{ import = "astrocommunity.completion.copilot-lua" },
+	{ import = "astrocommunity.file-explorer.telescope-file-browser-nvim" },
 }
