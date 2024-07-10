3a1d7682aa88540981e919a58e8241a8a466b59f
MatthewYe <sssyeke@gmail.com>
2024-07-10 11:09:04 +0800

    feat: add ruby support

--- init.lua
+++ init.lua
@@ -12,7 +12,7 @@ vim.opt.rtp:prepend(lazypath)
 if not pcall(require, "lazy") then
   -- stylua: ignore
   vim.api.nvim_echo(
-  { { ("Unable to load lazy from: %s\n"):format(lazypath), "ErrorMsg" }, { "Press any key to exit...", "MoreMsg" } },
+    { { ("Unable to load lazy from: %s\n"):format(lazypath), "ErrorMsg" }, { "Press any key to exit...", "MoreMsg" } },
     true, {})
   vim.fn.getchar()
   vim.cmd.quit()
@@ -24,6 +24,6 @@ require("polish")
 local auto_command_on = vim.api.nvim_create_autocmd
 
 auto_command_on({ "BufRead", "BufNewFile" }, {
-  pattern = { "*.podspec", "Podfile" },
+  pattern = { "*.podspec", "Podfile", ".Podfile.patch" },
   command = "set filetype=ruby",
 })
