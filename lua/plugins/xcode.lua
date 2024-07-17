b40f937c6180b5daa686116b45891b4cb4aa9331
MatthewYe <sssyeke@gmail.com>
2024-07-17 21:27:07 +0800

    feat:add xcode lua

---
+++ lua/plugins/xcode.lua
@@ -0,0 +1,22 @@
+-- if true then
+--   return {}
+-- end
+-- -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+
+--
+---@type LazySpec
+return {
+  "wojciech-kulik/xcodebuild.nvim",
+  dependencies = {
+    "nvim-telescope/telescope.nvim",
+    "MunifTanjim/nui.nvim",
+    "nvim-tree/nvim-tree.lua",       -- (optional) to manage project files
+    "stevearc/oil.nvim",             -- (optional) to manage project files
+    "nvim-treesitter/nvim-treesitter", -- (optional) for Quick tests support (required Swift parser)
+  },
+  config = function()
+    require("xcodebuild").setup({
+      -- put some options here or leave it empty to use default settings
+    })
+  end,
+}
